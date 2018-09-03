259a8230b6b0d1be797edab2c470afe90a760c08
Richard <rahayn@yahoo.com>
2018-09-03 15:24:57 -0400

    More layout Changes
    Added About message and Review sections

---
+++ main.js
@@ -0,0 +1,19 @@
+/*
+To use this code on your website, get a free API key from Google.
+Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
+*/
+function myMap()
+{
+  myCenter=new google.maps.LatLng(41.878114, -87.629798);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: false, draggable: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
